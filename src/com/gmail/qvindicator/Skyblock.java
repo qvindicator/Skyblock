57c1a0ee4baa6cdbec74c0cdd81ed671d4e93462
qvindicator <51041573+qvindicator@users.noreply.github.com>
2019-06-27 18:03:09 -0700

    Add files via upload

---
+++ src/com/gmail/qvindicator/Skyblock.java
@@ -0,0 +1,14 @@
+package com.gmail.qvindicator;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Skyblock extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
